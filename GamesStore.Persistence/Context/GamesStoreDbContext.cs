e69fba360b936d502a77a89a97fd5250c2998225
willianguilherme <williangui06@gmail.com>
2024-08-08 22:34:38 -0300

    feat: criando context

---
+++ GamesStore.Persistence/Context/GamesStoreDbContext.cs
@@ -0,0 +1,28 @@
+﻿using GamesStore.Persistence.Entities;
+using GamesStore.Persistence.IdTypes;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamesStore.Persistence.Context;
+
+public class GamesStoreDbContext : DbContext
+{
+    public GamesStoreDbContext(DbContextOptions<GamesStoreDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+        {
+            foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+
+        modelBuilder.Entity<User>(user =>
+        {
+            user.Property(u => u.Id)
+                .HasConversion(
+                    id => id.ToGuid(),
+                    id => new UserId(id));
+        });
+    }
+}
