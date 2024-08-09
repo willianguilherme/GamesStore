df505a9c0b44838e2537ee4ec72ca44737515aa1
willianguilherme <williangui06@gmail.com>
2024-08-08 22:34:15 -0300

    feat: criando ID para User

---
+++ GamesStore.Persistence/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using GamesStore.Persistence.IdTypes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GamesStore.Persistence.Entities;
+
+[Table("User")]
+public class User
+{
+    [Column("id")]
+    [Key]
+    public UserId Id { get; set; } = new UserId(Guid.Empty);
+
+    [Column("full_name")]
+    public string FullName { get; set; } = string.Empty;
+
+    [Column("email")]
+    public string Email { get; set; } = string.Empty;
+}
