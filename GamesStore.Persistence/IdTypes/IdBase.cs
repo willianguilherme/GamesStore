d6fcb8485a39e06cdc797b7733255c0cba7efb21
willianguilherme <williangui06@gmail.com>
2024-08-08 22:33:08 -0300

    feat: criando tipo base de IDs

---
+++ GamesStore.Persistence/IdTypes/IdBase.cs
@@ -0,0 +1,31 @@
+﻿namespace GamesStore.Persistence.IdTypes;
+
+public abstract record IdBase
+{
+    private Guid _value;
+
+    public IdBase(Guid value)
+    {
+        _value = value;
+    }
+
+    public IdBase()
+    {
+        _value = Guid.NewGuid();
+    }
+
+    public Guid ToGuid()
+    {
+        return _value;
+    }
+
+    public override string ToString()
+    {
+        return _value.ToString();
+    }
+
+    public bool IsEmpty()
+    {
+        return _value == Guid.Empty;
+    }
+}
