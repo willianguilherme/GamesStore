f8da6f1a7d89f95b938fe2ec21d60df238b79276
willianguilherme <williangui06@gmail.com>
2024-08-12 22:09:10 -0300

    feat: criando classe de informações do sistema

---
+++ GamesStore.API/Configurations/SystemConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace GamesStore.API.Configurations;
+
+public static class SystemConfiguration
+{
+    private static string _systemVersion { get; set; } = string.Empty;
+    public static string SystemVersion { get { return _systemVersion; } }
+
+    public static IServiceCollection GetSystemConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        string? systemVersion = configuration.GetSection("SystemInformation:SystemVersion").Value;
+
+        if (string.IsNullOrEmpty(systemVersion))
+        {
+            throw new InvalidOperationException("A configuração 'SystemInformation:SystemVersion' não foi encontrada ou está vazia.");
+        }
+
+        _systemVersion = systemVersion;
+        return services;
+    }
+}
