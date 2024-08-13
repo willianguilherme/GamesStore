77b31e1d713eb856b3c8839cedd1bc910d1dc221
willianguilherme <williangui06@gmail.com>
2024-08-12 21:37:17 -0300

    feat: configurando versionamento

--- GamesStore.API/Program.cs
+++ GamesStore.API/Program.cs
@@ -1,20 +1,34 @@
+using Asp.Versioning;
 using GamesStore.Application;
 using GamesStore.Infrastructure;
 using GamesStore.Persistence;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Games Store v1", Version = "v1" });
+    c.SwaggerDoc("v2", new OpenApiInfo { Title = "Games Store v2", Version = "v2" });
+});
 
 // Builder projects
 builder.Services.BuilderApplicationServices(builder.Configuration);
 builder.Services.BuilderInfrastructureServices(builder.Configuration);
 builder.Services.BuilderPersistenceServices(builder.Configuration);
+
+builder.Services.AddApiVersioning(option =>
+{
+    option.DefaultApiVersion = new ApiVersion(1, 0);
+    option.AssumeDefaultVersionWhenUnspecified = true;
+    option.ReportApiVersions = true;
+    option.ApiVersionReader = new UrlSegmentApiVersionReader();
+}).AddApiExplorer(options => {
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
 
 var app = builder.Build();
 
@@ -22,7 +36,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GamesStore_v1");
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "GamesStore_v2");
+    });
 }
 
 app.UseHttpsRedirection();
